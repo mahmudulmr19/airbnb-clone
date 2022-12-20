da88b21cd10c443a56069e907d29154fc5337797
Mahmudulmr17 <mahmudulmr17@gmail.com>
2022-12-20 11:15:34 +0600

    dhgiuergiuwrg

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function index({ exploreData, cardData }) {
     <div>
       <Head>
         <title>Vacation Homes & Condo Rentals - Airbnb - Airbnb</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <Header />
 
